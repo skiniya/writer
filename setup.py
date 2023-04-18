f48f9d180e3195fb68e0d808fd57548e5953a34f
rescued <resscued@gmail.com>
2023-04-18 20:12:53 +0300

    new: package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Writer',
+      author='skiniya',
+      author_email='',
+      url='',
+      packages=['faster_whisper'],
+     )
