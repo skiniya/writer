5d080d6419113cfc74e2585114b261288060d928
rescued <resscued@gmail.com>
2023-04-18 19:04:07 +0300

    init

---
+++ convert.py
@@ -0,0 +1,43 @@
+# from google.colab import files
+from faster_whisper import WhisperModel
+import fnmatch
+import os
+
+
+def convert(sec):
+    sec = sec % (24 * 3600)
+    hour = sec // 3600
+    sec %= 3600
+    min = sec // 60
+    sec %= 60
+    return "%02d:%02d:%02d" % (hour, min, sec)
+
+
+print("Сначала загружаем сюда наше аудио (для ПОЛНОГО написания), которое переименовано в цифры\n🔻🔻🔻🔻🔻\n")
+# uploaded = files.upload()
+for file in os.listdir('./'):
+    print(file)
+    if fnmatch.fnmatch(file, '*.mp3'):
+        dir: str = file
+        id_audio = dir[:-4]
+
+        model_size = "medium"  # лучшее воспроизведение текста
+        model = WhisperModel(model_size, device="cpu", compute_type="int8")
+        segments, info = model.transcribe(f"{id_audio}.mp3", beam_size=5)
+        print("*****************\nДлина аудио: %s (%dс) " % (convert(info.duration), info.duration))
+        print('*****************\nТекст аудио:')
+        myfile = open(f"{id_audio}.txt", "a")
+
+        for segment in segments:
+            myfile.write(segment.text)
+            print("[%s - %s] %s" % (convert(segment.start), convert(segment.end), segment.text))
+        myfile.close()
+
+        with open(f"{id_audio}.txt", 'r+') as myfile:
+            txt = myfile.read().replace('ё', 'е')
+            myfile.seek(0)
+            myfile.truncate()
+            myfile.write(txt)
+        myfile.close()
+
+        print("*****************\nГотово!\n✔️✔️✔️✔️✔️")
