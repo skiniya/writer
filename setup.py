72ba9f68be2740c7d77e67319b7d3839c61deffc
rescued <resscued@gmail.com>
2023-04-18 20:32:29 +0300

    new: package

--- setup.py
+++ setup.py
@@ -1,12 +1,44 @@
-#!/usr/bin/env python
+import os
+import platform
+import sys
 
-from distutils.core import setup
+import pkg_resources
+from setuptools import find_packages, setup
 
-setup(name='Writer',
-      version='1.0',
-      description='Writer',
-      author='skiniya',
-      author_email='',
-      url='',
-      packages=['converter'],
-     )
+
+# def read_version(fname="whisper/version.py"):
+#     exec(compile(open(fname, encoding="utf-8").read(), fname, "exec"))
+#     return locals()["__version__"]
+
+
+requirements = []
+# if sys.platform.startswith("linux") and platform.machine() == "x86_64":
+#     requirements.append("triton==2.0.0")
+
+setup(
+    name="writer",
+    py_modules=["whisper"],
+    # version=read_version(),
+    version=1.0,
+    description="Audio writer",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    readme="README.md",
+    python_requires=">=3.8",
+    author="skiniya",
+    url="",
+    license="MIT",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=requirements
+    + [
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ],
+    # entry_points={
+        # "console_scripts": ["whisper=whisper.transcribe:cli"],
+    # },
+    include_package_data=True,
+    # extras_require={"dev": ["pytest", "scipy", "black", "flake8", "isort"]},
+)
