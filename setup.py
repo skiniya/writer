6c384011ffdfe149ad9381cdd2316247939c1c03
liora <urichsvet@gmail.com>
2023-04-19 14:30:22 +0300

    writer: add part_converter.py

--- setup.py
+++ setup.py
@@ -37,7 +37,8 @@ setup(
     #     )
     # ],
     entry_points={
-        "console_scripts": ["converter=converter.full:cli"],
+        "console_scripts": ["converter=converter-1.full:cli"],["converter=delete.del:cli"],
+    ["converter=converter-2.part:cli"]
 
     },
     include_package_data=True,
