95faec69ac5fc3ea48b6654a0bce5295120456ab
liora <urichsvet@gmail.com>
2023-05-08 02:43:38 +0300

    writer: add part_converter.py

--- converter/__init__.py
+++ converter/__init__.py
@@ -1,14 +1,6 @@
-#from .full import convert
+from .full import convert, file, myfile
 
-print("Пакет Converter импортирован.")
-
-from .full import convert
-
-__all__ = ['convert']
-
-#from full import
-
-#print("Full audio")
+__all__ = ['convert','file','myfile']
 
 
 
