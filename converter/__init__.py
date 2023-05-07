18bfa9194c61f22e5e17b04f334251a2b0a4f496
liora <urichsvet@gmail.com>
2023-05-07 17:18:31 +0300

    writer: add part_converter.py

--- converter/__init__.py
+++ converter/__init__.py
@@ -1,3 +1,7 @@
 from .full import convert
 
 print("Full audio")
+
+if __name__ == '__full__':
+    # execute only if run as the entry point into the program
+    full()
--- converter/del.py
+++ converter/del.py
@@ -3,6 +3,9 @@ os.chdir("/content/")
 for file in glob.glob("*.mp3"):
     os.remove(file)
     print(f"{file} удален")
+for file in glob.glob("*.wav"):
+    os.remove(file)
+    print(f"{file} удален")
 for file in glob.glob("*.txt"):
     os.remove(file)
     print(f"{file} удален")    
