4b2f8c0e27e9ea494a0390db276f40c44f1be7d8
liora <urichsvet@gmail.com>
2023-05-08 01:42:52 +0300

    writer: add part_converter.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     url="",
     license="MIT",
     packages=find_packages(exclude=["tests*"]),
-    install_requires=["faster-whisper","pydub", "tqdm", "fnmatch"],
+    install_requires=["faster-whisper","pydub", "tqdm"],
     # install_requires=requirements
     # + [
     #     str(r)
