4f8e60328ed04c9b8692b2106afd4cd8d40ef86f
liora <urichsvet@gmail.com>
2023-05-09 00:56:31 +0300

    writer: add part_converter.py

--- converter/cut.py
+++ converter/cut.py
@@ -11,24 +11,24 @@ def cutAudio():
     for file in glob.glob("*.mp3"):
         print(file)
 
-        def time_to_sec(t1):
-           h, m, s = map(int, t1.split(':'))
-           return h * 3600 + m * 60 + s
-           print("Введите начальное время в формате hh:mm:ss")
-           t1 = input()
-           t11: int = time_to_sec(t1)
-        def time_to_sec(t2):
-           h, m, s = map(int, t2.split(':'))
-           return h * 3600 + m * 60 + s
-           print("Введите конечное время в формате hh:mm:ss")
-           t2 = input()
-           t22: int = time_to_sec(t2)
+    def time_to_sec(t1):
+        h, m, s = map(int, t1.split(':'))
+        return h * 3600 + m * 60 + s
+        print("Введите начальное время в формате hh:mm:ss")
+        t1 = input()
+        t11: int = time_to_sec(t1)
+    def time_to_sec(t2):
+        h, m, s = map(int, t2.split(':'))
+        return h * 3600 + m * 60 + s
+        print("Введите конечное время в формате hh:mm:ss")
+        t2 = input()
+        t22: int = time_to_sec(t2)
 
         print("Запускаю процесс...")
 
         t_start = t11 * 1000  # если нужно от начала - 0 * 1000
         t_end = t22 * 1000  # 60 секунд - до 1-й минуты
-        new_audio = AudioSegment.from_mp3(id_audio + ".mp3")
+        new_audio = AudioSegment.from_mp3(file)
         a = new_audio[t_start:t_end]
         a.export("fragment.mp3", format="mp3")
 
