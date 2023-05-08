c5cbc2ae7497dc477c1c8ba0fa7e9921d23b7954
liora <urichsvet@gmail.com>
2023-05-08 15:35:58 +0300

    writer: add part_converter.py

--- converter/__init__.py
+++ converter/__init__.py
@@ -1,11 +1,7 @@
-#from .full import convert
 
-#print('__init__ converter')
 
 print("Вас приветствует код по расшифровке аудио в текст.\n__________________________________________________\n")
 
-
-#from .full import *
 
 
 
--- setup.py
+++ setup.py
@@ -2,43 +2,21 @@ import os
 import platform
 import sys
 
-import pkg_resources
-
-from setuptools import find_packages, setup
-
-
-# def read_version(fname="whisper/version.py"):
-#     exec(compile(open(fname, encoding="utf-8").read(), fname, "exec"))
-#     return locals()["__version__"]
-
-requirements = []
-# if sys.platform.startswith("linux") and platform.machine() == "x86_64":
-#     requirements.append("triton==2.0.0")
+from setuptools import setup, find_packages
 
 setup(
-    name="writer",
-    py_modules=["whisper"],
-    # version=read_version(),
-    version=1.0,
-    description="Audio writer",
-    long_description=open("README.md", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    readme="README.md",
-    python_requires=">=3.8",
-    author="skiniya",
-    url="",
+    version="1.0",
+    name="Converter",
+    packages=find_packages(),
+    py_modules=["converter"],
     license="MIT",
-    packages=find_packages(exclude=["tests*"]),
-    install_requires=["faster-whisper","pydub", "tqdm"],
-    # install_requires=requirements
-    # + [
-    #     str(r)
-    #     for r in pkg_resources.parse_requirements(
-    #         open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
-    #     )
-    # ],
-    entry_points={"console_scripts": ["writer=writer.full:main"]}
-    #include_package_data=True,
-    # extras_require={"dev": ["pytest", "scipy", "black", "flake8", "isort"]},
+    author="Skiniya",
+    install_requires=[
+        'faster-whisper', 'pydub', 'tqdm'
+    ],
+    description="Automatically generate text into your audio",
+    entry_points={
+        'console_scripts': ['converter=converter.full:main'],
+    },
+    include_package_data=True,
 )
-
