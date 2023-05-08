e190a30c65475e47c09b99ba46184c168d8b94ca
liora <urichsvet@gmail.com>
2023-05-08 18:35:00 +0300

    writer: add part_converter.py

--- converter/__init__.py
+++ converter/__init__.py
@@ -1,7 +1,7 @@
 
 print("Вас приветствует код по расшифровке аудио.\n__________________________________________\n")
 
-from converter import full_audio
+from converter import full
 
 __all__ = ["full_audio"]
 
