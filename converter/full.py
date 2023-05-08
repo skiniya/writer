ff969f37c6eba9bf3693580fc96ab12265649516
liora <urichsvet@gmail.com>
2023-05-08 18:30:14 +0300

    writer: add part_converter.py

--- converter/full.py
+++ converter/full.py
@@ -2,7 +2,7 @@
 
 #from main import *
 
-def full():
+def full_audio():
 
 from faster_whisper import WhisperModel
 from tqdm import tqdm
@@ -74,6 +74,6 @@ else:
     print("\nНет загруженного аудио. Работа завершена.")
 
 if __name__ == '__main__':
-    full()
+    full_audio()
 else:
     print(f"main: {__name__}")
