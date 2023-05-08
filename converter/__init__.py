07b6fd1c25c1469be069a41d1ac2386002c2bd23
liora <urichsvet@gmail.com>
2023-05-08 18:18:34 +0300

    writer: add part_converter.py

--- converter/__init__.py
+++ converter/__init__.py
@@ -1,5 +1,5 @@
 
-#from .full import *
+from full import *
 
 
 
--- setup.py
+++ setup.py
@@ -7,14 +7,13 @@ from setuptools import setup, find_packages
 setup(
     version="1.0",
     name="converter",
+    scripts=['full.py'],
     packages=find_packages(),
     py_modules=["converter"],
     license="MIT",
     author="Skiniya",
     install_requires=['faster-whisper', 'pydub', 'tqdm'],
     description="Automatically generate text into your audio",
-    entry_points={
-        'console_scripts': ['converter=converter.full:main'],
-    },
+    #entry_points={'console_scripts': ['converter=converter.full:main']},
     include_package_data=True,
 )
