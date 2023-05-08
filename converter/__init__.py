c1ed1c4d485744835fa06d3f788f122749ee82d6
liora <urichsvet@gmail.com>
2023-05-08 14:35:47 +0300

    writer: add part_converter.py

--- converter/__init__.py
+++ converter/__init__.py
@@ -2,7 +2,7 @@
 
 #print('__init__ converter')
 
-print("Вас приветствует код по расшифровке аудио в текст.")
+print("Вас приветствует код по расшифровке аудио в текст.\n")
 
 import converter
 from .setup import *
